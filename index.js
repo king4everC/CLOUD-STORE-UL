1410cf21059d2eca9a2ffd1889487ae84db5a4ce
king4everC <163314110+king4everC@users.noreply.github.com>
2024-03-25 14:13:53 +0200

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify","Watching YouTube"];
+const statusMessages = ["We Are Cloud","Cloud-store Official BOT "];
 
 
 let currentIndex = 0;
